d707d01ed1425b43f62bf46f1b9fc975e6e634d2
Susanna Kline <susannakline@gmail.com>
2018-08-24 00:04:08 -0500

    Add new enemies

--- js/app.js
+++ js/app.js
@@ -1,43 +1,42 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    // x pos
-    this.x = 0;
-    // y pos
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+class Enemy {
+    constructor(ypos) {
+        //lateral move distance
+        this.lat = 101;
+        //vertical move distance
+        this.vert = 65;
+        this.x = -50;
+        this.y = this.vert;
+        this.sprite = 'images/enemy-bug.png';
+    }
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        //if enemy is not off the border
+        if (this.x < this.lat * 5) {
+            //Move forward and increment x by speed * dt for consistency across browsers
+            this.x += this.lat * dt;
+        }
+        //if enemy is past border reset enemy to original spot
+        else {
+           this.x = -50;
+        }
+    }
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
 
-    //If (enemy is not passed boundary) {
-        //Move forward
-        //Increment x by speed * dt
-    //}
-    //else {
-        //reset pos to start
-    //}
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
 
 // Create the player class
 class Player {
     constructor() {
         //lateral move distance
-        this.lat = (101);
+        this.lat = 101;
         //vertical move distance
-        this.vert = (83);
+        this.vert = 83;
         this.startX = this.lat;
         this.startY = this.vert * 5 - 7;
         this.x = this.startX;
@@ -52,8 +51,7 @@ class Player {
                 //Did player x and y equal final tiles              
     }
     render() {
-        // Draw player sprite on current x and y coordinate position
-        // copied from Enemy class
+        // Draw player sprite on current x and y coordinate position, copied from Enemy class
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);       
     }
     handleInput(key) {
@@ -92,6 +90,8 @@ class Player {
     resetPlayer() {
         console.log('Player resetPlayer works');
         //set x and y to starting point x and y 
+        this.x = this.startX;
+        this.y = this.startY;
     }
 }
 
@@ -103,8 +103,25 @@ class Player {
 const player = new Player();
 
 //Init allEnemies array
-allEnemies = [];
+const allEnemies = [];
+
 //For each enemy create and push new Enemy object into above array
+const enemy1 = new Enemy();
+const enemy2 = new Enemy();
+const enemy3 = new Enemy();
+const enemy4 = new Enemy();
+allEnemies.push(enemy1);
+
+
+
+//for (let i = 0; i < 4; i++) {
+//    setTimeout(function() {
+//        allEnemies.push(new Enemy());
+//    }, 2000);
+//}
+
+
+
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
